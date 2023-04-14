d4e001ab77f43d239945a49d052cb50e9062c9f8
YoooJuno <juno980220@naver.com>
2023-04-14 19:18:59 +0900

    knapsack

--- _KNAPSACK/boj12865.c
+++ _KNAPSACK/boj12865.c
@@ -1,9 +1,22 @@
+// https://www.acmicpc.net/problem/12865
+
 #include <stdio.h>
 
 
-
-
 int main(){
+
+    int N; // (1 ≤ N ≤ 100)
+    int K; // (1 ≤ K ≤ 100,000)
+    scanf("%d %d" , &N , &K);
+
+    int W[N]; // (1 ≤ W ≤ 100,000)
+    int V[N]; // (0 ≤ V ≤ 1,000)
+
+    for(int i=0 ; i<N ; i++){
+        scanf("%d %d" , &W[i] , &V[i]);
+    }
+
+
 
 
 
